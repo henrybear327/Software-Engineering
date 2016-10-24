185992bec9008d204cd6d8fee2f60f762a5f6ab1
Chun-Hung Tseng <henrybear327@users.noreply.github.com>
2016-10-24 14:54:29 +0800

    Update index.php

--- Lab1/Smarty/smarty/index.php
+++ Lab1/Smarty/smarty/index.php
@@ -8,8 +8,30 @@
 
 <?php
 require("main.php");
-$hello = "Hello World!";
+$hello = "hello";
 $smarty->assign("hello",$hello);
+
+$str1 = "naitonal Chung Cheng University";
+$smarty->assign("str1", $str1);
+
+$str2="Computer and ";
+$smarty->assign("str2", $str2);
+
+$str3="Time";
+$smarty->assign("str3", $str3);
+
+//$str4
+$smarty->assign("str4", isset($str4) ? $str4 : null);
+
+$str5="add_8bit";
+$smarty->assign("str5", $str5);
+
+$str6="YOU CAN DO YOUR BEST !!!"; 
+$smarty->assign("str6", $str6);
+
+$str7="No pain,no gain.";
+$smarty->assign("str7", $str7);
+
 $smarty->display('index.tpl');
 ?>
 
